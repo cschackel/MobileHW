08df36e03e68fa3db08db0faf4223519956cb9f3
cschackel <cschackel@my.wctc.edu>
2020-01-22 17:18:57 -0600

    Updated to Include Ex 2

--- WebHW11/Program.cs
+++ WebHW11/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,15 +11,283 @@ namespace MobileLab1
     {
         static void Main(string[] args)
         {
+            bool running = true;
+
+            while (running)
+            {
+                Console.WriteLine("1: Grid Example\n2: Class Example\n3: Exit");
+
+                int program = getNumber(1, 3);
+
+                switch (program)
+                {
+                    case 1:
+                        doGridExample();
+                        break;
+                    case 2:
+                        doPersonExample();
+                        break;
+                    case 3:
+                        running = false;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+
+
+        }
+
+        private static void doPersonExample()
+        {
+            bool exRunning = true;
+
+            ArrayList people = new ArrayList();
+
+            while(exRunning)
+            {
+                Console.WriteLine($"1: Add Student\n 2: Add Teacher\n3: Exit");
+                int choice = getNumber(1, 3);
+                switch(choice)
+                {
+                    case 1:
+                        people.Add(addStudent());
+                        break;
+                    case 2:
+                        people.Add(addTeacher());
+                        break;
+                    case 3:
+                        exRunning = false;
+                        displayPeople(people);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private static void displayPeople(ArrayList people)
+        {
+            foreach(Person p in people)
+            {
+                Console.WriteLine("\n" + p.ToString() + "\n");
+            }
+        }
+
+        private static Teacher addTeacher()
+        {
+            Teacher teacher = new Teacher();
+            Console.Write("Name: ");
+            teacher.name = Console.ReadLine();
+
+            bool validAge = false;
+            while (!validAge)
+            {
+                Console.Write("Age: ");
+                string userNumStr = Console.ReadLine();
+                int userNum;
+                bool isNumber = int.TryParse(userNumStr, out userNum);
+                if (isNumber && userNum >= 0)
+                {
+                    validAge = true;
+                    teacher.age = userNum;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input");
+                }
+            }
+
+            Console.WriteLine("ID: ");
+            teacher.ID = Console.ReadLine();
+
+            Console.WriteLine("Program: \n" +
+                "1: Computer Science\n" +
+                "2: Accounting\n" +
+                "3: Marketing\n" +
+                "4: Nursing\n");
+            int program = getNumber(1, 4);
+            switch (program)
+            {
+                case 1:
+                    teacher.schoolProgram = SchoolProgram.Computer_Science;
+                    break;
+                case 2:
+                    teacher.schoolProgram = SchoolProgram.Accounting;
+                    break;
+                case 3:
+                    teacher.schoolProgram = SchoolProgram.Marketing;
+                    break;
+                case 4:
+                    teacher.schoolProgram = SchoolProgram.Nursing;
+                    break;
+                default:
+                    break;
+            }
+
+            bool validYr = false;
+            while (!validYr)
+            {
+                Console.Write("Years of Service: ");
+                string userNumStr = Console.ReadLine();
+                int userNum;
+                bool isNumber = int.TryParse(userNumStr, out userNum);
+                if (isNumber && userNum >= 0)
+                {
+                    validYr = true;
+                    teacher.yearsOfService = userNum;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input");
+                }
+            }
+
+            return teacher;
+        }
+
+        private static Student addStudent()
+        {
+            Student student = new Student();
+            Console.Write("Name: ");
+            student.name = Console.ReadLine();
+
+            bool validAge = false;
+            while(!validAge)
+            {
+                Console.Write("Age: ");
+                string userNumStr = Console.ReadLine();
+                int userNum;
+                bool isNumber = int.TryParse(userNumStr, out userNum);
+                if(isNumber && userNum >= 0)
+                {
+                    validAge = true;
+                    student.age = userNum;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input");
+                }
+            }
+
+            Console.WriteLine("ID: ");
+            student.ID = Console.ReadLine();
+
+            Console.WriteLine("Program: \n" +
+                "1: Computer Science\n" +
+                "2: Accounting\n" +
+                "3: Marketing\n" +
+                "4: Nursing\n");
+            int program = getNumber(1,4);
+            switch(program)
+            {
+                case 1:
+                    student.schoolProgram = SchoolProgram.Computer_Science;
+                    break;
+                case 2:
+                    student.schoolProgram = SchoolProgram.Accounting;
+                    break;
+                case 3:
+                    student.schoolProgram = SchoolProgram.Marketing;
+                    break;
+                case 4:
+                    student.schoolProgram = SchoolProgram.Nursing;
+                    break;
+                default:
+                    break;
+            }
+
+            bool validCr = false;
+            while (!validCr)
+            {
+                Console.Write("Credits Earned: ");
+                string userNumStr = Console.ReadLine();
+                int userNum;
+                bool isNumber = int.TryParse(userNumStr, out userNum);
+                if (isNumber && userNum >= 0)
+                {
+                    validCr = true;
+                    student.creditsEarned = userNum;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input");
+                }
+            }
+
+            return student;
+
+        }
+
+        public enum SchoolProgram
+        {
+            Computer_Science,
+            Accounting,
+            Marketing,
+            Nursing
+        }
+
+        public class Person
+        {
+            public string name { get; set; }
+            public string ID { get; set; }
+            public int age { get; set; }
+            public SchoolProgram schoolProgram { get; set; }
+
+            public virtual string ToString()
+            {
+                return $"Name: {name}\nID: {ID}\nAge: {age}\nProgram: {schoolProgram}";
+            }
+        }
+
+        public class Teacher:Person
+        {
+            public int yearsOfService { get; set; }
+
+            public override string ToString()
+            {
+                return base.ToString() + $"\nYears of Service: {yearsOfService}";
+            }
+        }
+
+        public class Student : Person
+        {
+            public int creditsEarned { get; set; }
+
+            public override string ToString()
+            {
+                return base.ToString() + $"\nCredits Earned: {creditsEarned}";
+            }
+        }
+
+        private static void displayRDiagFlip(int gridSize)
+        {
+            int[,] output = new int[5, 5];
+
+
+            for (int i = 1; i <= gridSize; i++)
+            {
+                for (int j = 1; j <= gridSize; j++)
+                {
+                    output[j - 1, i - 1] = j * i;
+                }
+            }
+
+            displayArray(output);
+        }
+
+        private static void doGridExample()
+        {
             int gridSize = getNumber(3, 9);
 
             int[,] grid = generateGrid(gridSize);
 
             displayGrid(gridSize);
 
-            bool running = true;
+            bool exRunning = true;
 
-            while (running)
+            while (exRunning)
             {
                 displayMenu();
 
@@ -39,32 +308,13 @@ namespace MobileLab1
                         displayRDiagFlip(gridSize);
                         break;
                     case 5:
-                        running = false;
+                        exRunning = false;
                         break;
                     default:
                         break;
 
                 }
             }
-
-
-
-        }
-
-        private static void displayRDiagFlip(int gridSize)
-        {
-            int[,] output = new int[5, 5];
-
-
-            for (int i = 1; i <= gridSize; i++)
-            {
-                for (int j = 1; j <= gridSize; j++)
-                {
-                    output[j - 1, i - 1] = j * i;
-                }
-            }
-
-            displayArray(output);
         }
 
 
